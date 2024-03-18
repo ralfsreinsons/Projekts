efed6c10c15e9ac93be1b2667d7a777d640ed5ac
ralfsreinsons <ralfsreinsons64@gmail.com>
2024-03-18 09:57:54 +0200

    Izvada liakapstākļus ievadītajai pilsētai

--- Laikazinas.py
+++ Laikazinas.py
@@ -1,9 +1,10 @@
 import requests
 pilseta = input("Ievadiet pilsetu:")
-geo= requests.get('http://api.openweathermap.org/geo/1.0/direct?q={pilseta}&limit=5&appid=d5f36631cc643ceb4aca81a4466d58e1').json()[0]
-kord= [geo['lat'], geo['lon']]
-print(geo)
+geo= requests.get('http://api.openweathermap.org/geo/1.0/direct?q=' + pilseta +'&limit=1&appid=d5f36631cc643ceb4aca81a4466d58e1').json()[0]
 
+lat= geo['lat']  
+lon= geo['lon']
 
-laikapst= requests.get('https://api.tomorrow.io/v4/weather/forecast?location={kord}&apikey=daDpfCTk68foELkxKjdeWq6WyPSTEdCC').json()
+laikapst= requests.get('https://api.tomorrow.io/v4/weather/forecast?location=' + str(lat) + ',' +  str(lon) +'&apikey=daDpfCTk68foELkxKjdeWq6WyPSTEdCC').json()
 
+print(laikapst)
