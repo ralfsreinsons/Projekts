97e2641cb9aebb633d08a7c3479f55d09b2d3cc0
ralfsreinsons <ralfsreinsons64@gmail.com>
2024-03-18 09:04:20 +0200

    Pirmais iesniegums

---
+++ Laikazinas.py
@@ -0,0 +1,9 @@
+import requests
+pilseta = input("Ievadiet pilsetu:")
+geo= requests.get('http://api.openweathermap.org/geo/1.0/direct?q={pilseta}&limit=5&appid=d5f36631cc643ceb4aca81a4466d58e1').json()[0]
+# kord= [geo['lat'], geo['lon']]
+print(geo)
+
+
+# laikapst= requests.get('https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=daDpfCTk68foELkxKjdeWq6WyPSTEdCC').json()
+
