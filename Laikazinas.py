2e9cf9786f6b6b42db0605b4333594e1f7100267
ralfsreinsons <ralfsreinsons64@gmail.com>
2024-03-22 13:02:28 +0200

    saullēta un saulrieta api pēc UTC laika joslas

--- Laikazinas.py
+++ Laikazinas.py
@@ -5,6 +5,7 @@ geo= requests.get('http://api.openweathermap.org/geo/1.0/direct?q=' + pilseta +'
 lat= geo['lat']  
 lon= geo['lon']
 
-laikapst = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + str(lat)+'&lon='+ str(lon)+ '&units=metric&appid=d5f36631cc643ceb4aca81a4466d58e1').json()
-
-print(laikapst)
+# laikapst = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + str(lat)+'&lon='+ str(lon)+ '&units=metric&appid=d5f36631cc643ceb4aca81a4466d58e1').json()
+saule = requests.get('https://api.sunrise-sunset.org/json?lat='+ str(lat)+'&lng=' + str(lon) +'&date=today').json()
+# print(laikapst)
+print(saule)
