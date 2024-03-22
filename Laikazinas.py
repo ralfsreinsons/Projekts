c71aa85943b55111771f83bc0d0b859829255dd4
ralfsreinsons <ralfsreinsons64@gmail.com>
2024-03-22 12:35:28 +0200

    Nomaina Laikaziņu api no tommorowio uz openweathermap

--- Laikazinas.py
+++ Laikazinas.py
@@ -5,6 +5,6 @@ geo= requests.get('http://api.openweathermap.org/geo/1.0/direct?q=' + pilseta +'
 lat= geo['lat']  
 lon= geo['lon']
 
-laikapst= requests.get('https://api.tomorrow.io/v4/weather/forecast?location=' + str(lat) + ',' +  str(lon) +'&apikey=daDpfCTk68foELkxKjdeWq6WyPSTEdCC').json()
+laikapst = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + str(lat)+'&lon='+ str(lon)+ '&units=metric&appid=d5f36631cc643ceb4aca81a4466d58e1').json()
 
 print(laikapst)
