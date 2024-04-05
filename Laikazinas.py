6b4c01800160d7a6a2d91def668c99b1af601ba2
ralfsreinsons <ralfsreinsons64@gmail.com>
2024-04-05 13:09:41 +0300

    laika prognozu izvele

--- Laikazinas.py
+++ Laikazinas.py
@@ -5,10 +5,18 @@ geo= requests.get('http://api.openweathermap.org/geo/1.0/direct?q=' + pilseta +'
 lat= geo['lat']  
 lon= geo['lon']
 
-laikapst = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + str(lat)+'&lon='+ str(lon)+ '&units=metric&appid=d5f36631cc643ceb4aca81a4466d58e1').json()
+laikazinas = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + str(lat)+'&lon='+ str(lon)+ '&units=metric&appid=d5f36631cc643ceb4aca81a4466d58e1').json()
 saule = requests.get('https://api.sunrise-sunset.org/json?lat='+ str(lat)+'&lng=' + str(lon) +'&date=today').json()['results']
-print(laikapst)
+
 saullekts = saule['sunrise']
 saulriets= saule['sunset']
 dienas_gar = saule['day_length']
+temp= 1
+piej_laikapstakli= {'temperatūra', 'temperatūra pēc jūtām', 'gaisa mitrums', 'nokrišņi', "vēja ātrums, brāzma un virziens"}
+
+
+for i in range(16):
+    print(laikazinas['list'][i]['main']['temp'])
+
+
 
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+piej_laikapstakli= {'temperatūra': temp, 'temperatūra pēc jūtām', 'gaisa mitrums', 'nokrišņi', 'vēja ātrums, brāzma un virziens'}
