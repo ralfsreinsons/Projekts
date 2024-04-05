8a4bbfb2f360b8dd99ed9fb1a3bee18c12f8af34
ralfsreinsons <ralfsreinsons64@gmail.com>
2024-04-05 11:01:27 +0300

    saullekta api

--- Laikazinas.py
+++ Laikazinas.py
@@ -5,7 +5,10 @@ geo= requests.get('http://api.openweathermap.org/geo/1.0/direct?q=' + pilseta +'
 lat= geo['lat']  
 lon= geo['lon']
 
-# laikapst = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + str(lat)+'&lon='+ str(lon)+ '&units=metric&appid=d5f36631cc643ceb4aca81a4466d58e1').json()
-saule = requests.get('https://api.sunrise-sunset.org/json?lat='+ str(lat)+'&lng=' + str(lon) +'&date=today').json()
-# print(laikapst)
-print(saule)
+laikapst = requests.get('http://api.openweathermap.org/data/2.5/forecast?lat=' + str(lat)+'&lon='+ str(lon)+ '&units=metric&appid=d5f36631cc643ceb4aca81a4466d58e1').json()
+saule = requests.get('https://api.sunrise-sunset.org/json?lat='+ str(lat)+'&lng=' + str(lon) +'&date=today').json()['results']
+print(laikapst)
+saullekts = saule['sunrise']
+saulriets= saule['sunset']
+dienas_gar = saule['day_length']
+
